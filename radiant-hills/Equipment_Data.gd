6eb4a10450df24a8f59c216ac9098a54712ac0f2
Dawson Turska <dawsonturska@gmail.com>
2024-10-26 14:20:00 -0400

    added equipment stats to combatant_1 that can be modified by allocating equipment into the designated slot via the Inspector.
    eventual goals:
    make it so the player can equip items in game somehow

---
+++ radiant-hills/Equipment_Data.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+@export var health_bonus: int = 0
+@export var attack_bonus: int = 0
+@export var slot_type: String
--- radiant-hills/combatant_stats.gd
+++ radiant-hills/combatant_stats.gd
@@ -1,5 +1,40 @@
 extends Sprite2D
 
+
+# Basic Stats
 var health = 100
 var attack_power = 10
 var is_attacking = false
+
+# Equipment Slots
+@export var Ring1: Resource
+@export var Ring2: Resource
+@export var Chest: Resource
+@export var Hat: Resource
+@export var Staff: Resource
+@export var Boots: Resource
+@export var Spellbook: Resource
+
+# Update Stats based on equipped items
+func set_stats():
+	# Base Stats
+	health = 100
+	attack_power = 10
+	
+	# Apply modifiers based off of equipment slots
+	if Ring1: apply_item_modifiers(Ring1)
+	if Ring2: apply_item_modifiers(Ring2)
+	if Chest: apply_item_modifiers(Chest)
+	if Hat: apply_item_modifiers(Hat)
+	if Staff: apply_item_modifiers(Staff)
+	if Boots: apply_item_modifiers(Boots)
+	if Spellbook: apply_item_modifiers(Spellbook)
+
+	print ("Updated player stats with equipment: Health - " + str(health) + ", Attack Power - " +str(attack_power))
+
+# Applies individual item modifiers.
+func apply_item_modifiers(item):
+	if item.has("health_bonus"):
+		health += item.health_bonus
+	if item.has("attack_bonus"):
+		attack_power += item.attack_bonus
