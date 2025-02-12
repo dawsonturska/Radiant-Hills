a55425554a3a3b942d5e3d9ca3c41933eb90c97d
KENT\dturska1 <dturska1@kent.edu>
2025-02-11 21:38:37 -0500

    added the Buttons resource as a means for currency, and working on Customer AI in the Shop scene

--- Radiant Hills/Assets/Scripts/Inventory/MaterialType.cs
+++ Radiant Hills/Assets/Scripts/Inventory/MaterialType.cs
@@ -5,4 +5,5 @@ public class MaterialType : ScriptableObject
 {
     public string materialName; // Name of the material
     public Sprite icon; // Icon for the material (set this in the inspector)
+    public float price;
 }
