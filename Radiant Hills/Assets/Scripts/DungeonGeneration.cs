89597813b989747b0fe09c89f0ba6cdbd5e16ce6
darienyoder <116597751+darienyoder@users.noreply.github.com>
2025-06-14 16:52:52 -0400

    Generation works; now needs Unity implementation

--- Radiant Hills/Assets/Scripts/DungeonGeneration.cs
+++ Radiant Hills/Assets/Scripts/DungeonGeneration.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DungeonGeneration : MonoBehaviour
 {
     // Dungeon layout
-    int[,] map;
+    public int[,] map;
 
     // Values used to fill tiles of "map"
-    int
+    public int
         // Unfilled tiles
         EMPTY = 0,
         // Unfilled tiles connected to filled tiles
@@ -20,15 +20,18 @@ public class DungeonGeneration : MonoBehaviour
         // one for each direction.
         // For example, a room with exits leading upward
         // and to the right has a value of 9 (1 + 8).
-        LEFT = 1,
-        RIGHT = 2,
-        UP = 4,
-        DOWN = 8;
+        LEFT  = 1,    LEFT_BIT  = 0,
+        RIGHT = 2,    RIGHT_BIT = 1,
+        UP    = 4,    UP_BIT    = 2,
+        DOWN  = 8,    DOWN_BIT  = 3,
+        
+        // SPECIAL ROOMS (No door bits)
+        BOSS_ROOM = 16;
 
     public int dungeon_bounds = 10;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         Generate();
     }
@@ -44,55 +47,215 @@ public class DungeonGeneration : MonoBehaviour
 
     }
 
-    void Generate(int max_rooms = 10, int seed = 0)
+
+    // Use these getters and setters 
+    // to access the door bits of a tile.
+
+    // Ex. GetBit(map[2,4], UP_BIT);
+    //      - Returns whether the tile
+    //        (2, 4) has a door leading
+    //        upward.
+    public bool GetBit(int number, int bit)
     {
-        Random.seed = seed;
+        return number / (int)Math.Pow(2, bit) % 2 == 1;
+    }
 
-        // Initialize dungeon grid
-        map = new int[dungeon_bounds, dungeon_bounds];
+    // Ex. map[2,4] = SetBit(map[2,4], UP_BIT) 
+    //      - Declares that the tile
+    //        (2, 4) has a door leading
+    //        upward.
+    public int SetBit(int number, int bit, bool value)
+    {
+        return (int)( (int) (number / Math.Pow(2, bit + 1)) * (int)Math.Pow(2, bit + 1) + Math.Pow(2, bit) * (value ? 1 : 0) + number % (int)Math.Pow(2, bit) );
+    }
 
-        // Set origin point
-        map[dungeon_bounds / 2, dungeon_bounds - 1] = OPEN; // The spawn room
-        map[dungeon_bounds / 2, dungeon_bounds - 2] = UP; // The exit (only accessible from spawn room)
-
-        int used_rooms = 0;
-        // Repeat until enough rooms are placed
-        while (used_rooms < max_rooms)
+    public void Generate(int max_rooms = 10, int seed = 0)
+    {
+        Random random = new Random();
+		bool is_valid = false;
+        
+        while (!is_valid)
         {
-            // Queueing phase
-            for (int x = 0; x < dungeon_bounds; x++)
-                for (int y = 0; y < dungeon_bounds; y++)
-                    if (map[x, y] == OPEN)
-                    {
-                        map[x, y] = QUEUED;
-                        used_rooms += 1;
-                    }
+            // Initialize dungeon grid
+            map = new int[dungeon_bounds, dungeon_bounds];
 
-            // Placing phase
-            for (int x = 0; x < dungeon_bounds; x++)
-                for (int y = 0; y < dungeon_bounds; y++)
-                    if (map[x, y] == QUEUED)
-                    {
-                        int door_value = 0;
+            // Set origin point
+            map[dungeon_bounds / 2, dungeon_bounds - 2] = OPEN; // The spawn room
+            map[dungeon_bounds / 2, dungeon_bounds - 1] = UP; // The exit (only accessible from spawn room)
 
-                        if (x != 0)
+            int used_rooms = 0;
+            // Repeat until enough rooms are placed
+            while (used_rooms < max_rooms)
+            {
+            	int rooms_added = 0;
+                // Queueing phase
+                // Open tiles are changed to queued tiles
+                for (int x = 0; x < dungeon_bounds; x++)
+                    for (int y = 0; y < dungeon_bounds; y++)
+                        if (map[x, y] == OPEN && used_rooms < max_rooms)
                         {
-                            // This assumes all surrounding tiles are filled
-                            // First check if tiles are filled, then do something
-                            // else if they are not.
-                            door_value += LEFT * (map[x - 1, y] / 1 % 2);
+                            map[x, y] = QUEUED;
+                            used_rooms += 1;
+                            rooms_added += 1;
+                            if (used_rooms == max_rooms)
+                            	map[x, y] = BOSS_ROOM;
                         }
-                        if (x != dungeon_bounds - 1)
-                            door_value += RIGHT * (map[x + 1, y] / 2 % 2);
-                        if (y != 0)
-                            door_value += UP * (map[x, y - 1] / 4 % 2);
-                        if (y != dungeon_bounds - 1)
-                            door_value += DOWN * (map[x, y + 1] / 8 % 2);
+                if (rooms_added == 0)
+                	break;
 
-                        map[x, y] = door_value;
+                // Placing phase
+                // Queued tiles are changed to filled tiles
+                for (int x = 0; x < dungeon_bounds; x++)
+                    for (int y = 0; y < dungeon_bounds; y++)
+                        if (map[x, y] == QUEUED)
+                        {
+                            int door_value = 0;
+
+                            if (x != 0)
+                            {
+                                if (map[x - 1, y] > 0)
+                                {
+                                    if (GetBit(map[x - 1, y], RIGHT_BIT))
+                                        door_value += LEFT;
+                                }
+                                else if (random.Next(3) != 0)
+                                {
+                                    door_value += LEFT;
+                                    map[x - 1, y] = OPEN;
+                                }
+
+                            }
+                            if (x != dungeon_bounds - 1)
+                            {
+                                if (map[x + 1, y] > 0)
+                                {
+                                    if (GetBit(map[x + 1, y], LEFT_BIT))
+                                        door_value += RIGHT;
+                                }
+                                else if (random.Next(3) != 0)
+                                {
+                                    door_value += RIGHT;
+                                    map[x + 1, y] = OPEN;
+                                }
+                            }
+                            if (y != 0)
+                            {
+                                if (map[x, y - 1] > 0)
+                                {
+                                    if (GetBit(map[x, y - 1], DOWN_BIT))
+                                        door_value += UP;
+                                }
+                                else if (random.Next(3) != 0)
+                                {
+                                    door_value += UP;
+                                    map[x, y - 1] = OPEN;
+                                }
+                            }
+                            if (y != dungeon_bounds - 1)
+                            {
+                                if (map[x, y + 1] > 0)
+                                {
+                                    if (GetBit(map[x, y + 1], UP_BIT))
+                                        door_value += DOWN;
+                                }
+                                else if (random.Next(3) != 0)
+                                {
+                                    door_value += DOWN;
+                                    map[x, y + 1] = OPEN;
+                                }
+                            }
+
+                            map[x, y] = door_value;
+                        }
+            	
+            }
+            is_valid = used_rooms == max_rooms;
+        }
+
+        // Cleanup phase
+        // Remove any doors on the outside of the level
+        // that don't lead anywhere
+        for (int x = 0; x < dungeon_bounds; x++)
+            for (int y = 0; y < dungeon_bounds; y++)
+                if (map[x, y] > 0 && map[x, y] < 16)
+                {
+                    if (x != 0)
+                    {
+                        map[x, y] = SetBit(map[x, y], LEFT_BIT, GetBit(map[x, y], LEFT_BIT) && map[x - 1, y] > 0);
                     }
+                    if (x != dungeon_bounds - 1)
+                    {
+                        map[x, y] = SetBit(map[x, y], RIGHT_BIT, GetBit(map[x, y], RIGHT_BIT) && map[x + 1, y] > 0);
+                    }
+                    if (y != 0)
+                    {
+                        map[x, y] = SetBit(map[x, y], UP_BIT, GetBit(map[x, y], UP_BIT) && map[x, y - 1] > 0);
+                    }
+                    if (y != dungeon_bounds - 1)
+                    {
+                        map[x, y] = SetBit(map[x, y], DOWN_BIT, GetBit(map[x, y], DOWN_BIT) && map[x, y + 1] > 0);
+                    }
+                }
+        
+        PrintDungeon();
+    }
 
-            // 
+    public void PrintDungeon()
+    {
+        Console.WriteLine("Dungeon Layout:");
+
+        // Find the first row of the dungeon that is
+        // filled so that we don't waste space printing the
+        // empty rows above that.
+        int top;
+        bool found = false;
+        for (top = 0; top < dungeon_bounds; top++)
+        {
+            for (int x = 0; x < dungeon_bounds; x++)
+                if (map[x, top] > 0)
+                {
+                    found = true;
+                    break;
+                }
+            if (found)
+                break;
+        }
+
+        for (int y = top; y < dungeon_bounds; y++)
+        {
+            string line = "";
+            for (int x = 0; x < dungeon_bounds; x++)
+            {
+                line += " ";
+                line += (map[x, y] > 0 && GetBit(map[x, y], 2)) ? "|" : " ";
+                line += " ";
+            }
+            Console.WriteLine(line);
+            line = "";
+            
+            for (int x = 0; x < dungeon_bounds; x++)
+            {
+                line += (map[x, y] > 0 && GetBit(map[x, y], 0)) ? "-" : " ";
+                if (x == dungeon_bounds / 2 && y == dungeon_bounds - 2)
+                	line += "S";
+                else if (x == dungeon_bounds / 2 && y == dungeon_bounds - 1)
+                	line += " ";
+                else if (map[x, y] == BOSS_ROOM)
+                	line += "B";
+                else
+	                line += (map[x, y] > 0) ? "#" : " ";
+                line += (map[x, y] > 0 && GetBit(map[x, y], 1)) ? "-" : " ";
+            }
+            Console.WriteLine(line);
+            line = "";
+               
+            for (int x = 0; x < dungeon_bounds; x++)
+            {
+                line += " ";
+                line += (map[x, y] > 0 && GetBit(map[x, y], 3)) ? "|" : " ";
+                line += " ";
+            }
+            Console.WriteLine(line);
         }
     }
 }
